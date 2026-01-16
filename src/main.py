0d760f2c6d231c12060cb3f3b1f0cc84ba5f30e6
Christian Jakubzig <wasserballer1@hotmail.de>
2026-01-16 19:07:14 +0100

    add dataset download

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from cnn-digit-recognition-project!")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
 
+kwargs = {'num_workers' : 1, 'pin_memory' : True}
 
-if __name__ == "__main__":
-    main()
+'''
+
+'''
+train = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "data",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1037,), (0.3081))],
+        ),
+    ), batch_size=64, shuffle=True, **kwargs
+)
+
+test = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "data",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1037,), (0.3081))],
+        ),
+    ), batch_size=64, shuffle=True, **kwargs
+)
