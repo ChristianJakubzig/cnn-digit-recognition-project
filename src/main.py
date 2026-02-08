bb8d4a5a07477061d79a57afd83ad076286cf134
Christian Jakubzig <wasserballer1@hotmail.de>
2026-02-08 19:26:18 +0100

    add the network layers

--- src/main.py
+++ src/main.py
@@ -31,6 +31,34 @@ test_data = torch.utils.data.DataLoader(
 )
 
 # ----------------------- Training -----------------------
+class Netz(nn.Module):
+    def __init__(self):
+        super(Netz, self).__init__()
+        self.convl = nn.Conv2d(1, 10, kernel_size=5) # Erste Schicht convolutional Layer, es werden immer 25 Pixel zusammengefasst auf einen 
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5) # Zweite schicht -> Nochmal mit dem Output aus 1
+        self.conf_dropout = nn.Dropout2d() # Hiermit können Pixel vergessen werden. Sonst lernt das Netz nur auswendig
+        self.fcl1 = nn.Linear(320, 60)
+        self.fcl2 = nn.Linear(60, 10) # zehn Outputs da wir 10 Möglichkeiten haben was erkannt werden kann
+
+    def forward(self, x):
+        x = self.convl(x)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x) # aktivierungsfunktion die besonders für convolutional Layers gut ist
+        x = self.conv2(x)
+        x = self.conf_dropout(x)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+        # -------Schritt zum testen ----------
+        # print(x.size()) # Ergebnis = torch.Size([64, 20, 4, 4]) 64 = Bilder da Batshsize = 64, 20 Haben wir als Output festgelegt in conv2, 4X4 ist die Bildgröße in die wir aufgeteilt haben -> 20 x 4 x 4 = 320 deswegen 320 in fcl1
+        # exit()
+        x = x.view(-1, 320)
+        x = F.relu(self.fcl1(x))
+        x = self.fcl2(x)
+        # return F.log_softmax(x, dim=1) # es gibt nur einen Gewinner alle anderen werden auf 0 gesetzt (geht irgendwie nicht)
+        return x
+
+model = Netz()
+model.cuda()
 
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.8)
 
@@ -43,10 +71,14 @@ def train(epoch):
         target = Variable(target)
         optimizer.zero_grad() # setzt alle Gradienten auf Null -> Muss jedes mal passieren / Aktivierungsfunktion
         out = model(data)
-        criterion = nn.CrossEntropyLoss # hier geht auch SoftMax
+        criterion = nn.CrossEntropyLoss() # hier geht auch SoftMax
         loss = criterion(out, target)
         loss.backward()
         optimizer.step()
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_id * len(data), len(train_data.dataset),
+            100. * batch_id / len(train_data), loss.item()
+        ))
 
 for epoch in range(1, 30):
     train(epoch)
